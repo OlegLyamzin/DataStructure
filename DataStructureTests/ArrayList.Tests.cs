703d537646cbc9a7f843bd4b42304c2ab68226ae
Oleg Lyamzin <o.s.like@yandex.ru>
2020-11-07 19:43:31 +0300

    Rename Mocks

--- DataStructureTests/ArrayList.Tests.cs
+++ DataStructureTests/ArrayList.Tests.cs
@@ -7,8 +7,8 @@ namespace DataStructureTests
     public class Tests
     {
         
-        [TestCase(6,1,1)]
-        public void AddTest(int value, int nArrayListMock, int nExpectedMock)
+        [TestCase(6,"FirstActualMock", "FirstExpectedMockForAdd")]
+        public void AddTest(int value, string nArrayListMock, string nExpectedMock)
         {
             ArrayList actual = GetActualArrayListMock(nArrayListMock);
             ArrayList expected = GetExpectedArrayListMock(nExpectedMock);
@@ -16,8 +16,8 @@ namespace DataStructureTests
             Assert.AreEqual(expected, actual);
         }
 
-        [TestCase(6,1,2)]
-        public void AddFirstTest(int value, int nArrayListMock, int nExpectedMock)
+        [TestCase(6, "FirstActualMock", "FirstExpectedMockForAddFirst")]
+        public void AddFirstTest(int value, string nArrayListMock, string nExpectedMock)
         {
             ArrayList actual = GetActualArrayListMock(nArrayListMock);
             ArrayList expected = GetExpectedArrayListMock(nExpectedMock);
@@ -25,8 +25,8 @@ namespace DataStructureTests
             Assert.AreEqual(expected, actual);
         }
 
-        [TestCase(6,3,1,3)]
-        public void AddByIndexTest(int value,int index, int nArrayListMock, int nExpectedMock)
+        [TestCase(6,3, "FirstActualMock", "FirstExpectedMockForAddByIndex")]
+        public void AddByIndexTest(int value,int index, string nArrayListMock, string nExpectedMock)
         {
             ArrayList actual = GetActualArrayListMock(nArrayListMock);
             ArrayList expected = GetExpectedArrayListMock(nExpectedMock);
@@ -34,8 +34,8 @@ namespace DataStructureTests
             Assert.AreEqual(expected, actual);
         }
 
-        [TestCase(1, 4)]
-        public void DeleteEndTest( int nArrayListMock, int nExpectedMock)
+        [TestCase("FirstActualMock", "FirstExpectedMockForDeleteEnd")]
+        public void DeleteEndTest( string nArrayListMock, string nExpectedMock)
         {
             ArrayList actual = GetActualArrayListMock(nArrayListMock);
             ArrayList expected = GetExpectedArrayListMock(nExpectedMock);
@@ -43,21 +43,21 @@ namespace DataStructureTests
             Assert.AreEqual(expected, actual);
         }
 
-        private ArrayList GetExpectedArrayListMock(int nExpectedMock)
+        private ArrayList GetExpectedArrayListMock(string nExpectedMock)
         {
             int[] array;
             switch (nExpectedMock)
             {
-                case 1:
+                case "FirstExpectedMockForAdd":
                     array = new int[] { 1, 2, 3, 4, 5, 6 };
                     return new ArrayList(array);
-                case 2:
+                case "FirstExpectedMockForAddFirst":
                     array = new int[] { 6, 1, 2, 3, 4, 5};
                     return new ArrayList(array);
-                case 3:
+                case "FirstExpectedMockForAddByIndex":
                     array = new int[] {1, 2, 3, 6, 4, 5 };
                     return new ArrayList(array);
-                case 4:
+                case "FirstExpectedMockForDeleteEnd":
                     array = new int[] { 1, 2, 3, 4 };
                     return new ArrayList(array);
                 default:
@@ -65,12 +65,12 @@ namespace DataStructureTests
             }
         }
 
-        private ArrayList GetActualArrayListMock(int nArrayListMock)
+        private ArrayList GetActualArrayListMock(string nArrayListMock)
         {
             int[] array;
             switch (nArrayListMock)
             {
-                case 1:
+                case "FirstActualMock":
                     array = new int[] { 1, 2, 3, 4, 5 };
                     return new ArrayList(array);                    
                 default:
