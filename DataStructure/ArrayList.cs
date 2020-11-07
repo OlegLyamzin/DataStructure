99e84b72dd0d46f8d8e36e55918acb1c61178007
Oleg Lyamzin <o.s.like@yandex.ru>
2020-11-07 13:18:33 +0300

    Start project

---
+++ DataStructure/ArrayList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace DataStructure
+{
+  public class ArrayList
+  {
+
+    public int Length { get; private set; }
+
+    private int[] _array;
+
+    public ArrayList()
+    {
+      _array = new int[9];
+      Length = 0;
+    }
+
+    public void Add(int value)
+    {
+      if (_array.Length <= Length)
+      {      
+        IncreaseLenght();
+      }
+
+      _array[Length] = value;
+      Length++;
+    }
+
+    private void IncreaseLenght(int number=1)
+    {
+      int newLenght = Length;
+      while (newLenght <= Length + number)
+      {
+        newLenght = (int)(newLenght * 1.33 + 1);
+      }
+
+      int[] newArray = new int[newLenght];
+      Array.Copy(_array, newArray, _array.Length);
+
+      _array = newArray;
+    }
+
+    public override bool Equals(object obj)
+    {
+      ArrayList arrayList = (ArrayList)obj;
+
+      if(Length != arrayList.Length)
+      {
+        return false;
+      }
+      
+      for(int i = 0; i< Length; i++)
+      {
+        if (_array[i] != arrayList._array[i])
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+}
