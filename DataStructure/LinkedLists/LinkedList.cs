74211329d0af2d69108da6560c1cbf54f3bbb9ec
Oleg Lyamzin <o.s.like@yandex.ru>
2020-11-11 17:37:55 +0300

    Added indexators and override toString and Equals

--- DataStructure/LinkedLists/LinkedList.cs
+++ DataStructure/LinkedLists/LinkedList.cs
@@ -4,7 +4,104 @@ using System.Text;
 
 namespace DataStructure.LinkedLists
 {
-    class LinkedList
+    public class LinkedList
     {
+        public int Length { get; set; }
+
+        private Node _root;
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                Node tmp = _root;
+                for (int i = 1; i <= index; i++)
+                {
+                    tmp = tmp.Next;
+                }
+                return tmp.Value;
+            }
+            set
+            {
+                if (index < 0 || index >= Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                Node tmp = _root;
+                for (int i = 1; i <= index; i++)
+                {
+                    tmp = tmp.Next;
+                }
+                tmp.Value = value;
+            }
+        }
+
+        public LinkedList(int[] array)
+        {
+            if(array.Length != 0)
+            {
+                _root = new Node(array[0]);
+                Node tmp = _root;
+
+                for(int i = 1; i < array.Length; i++)
+                {
+                    tmp.Next = new Node(array[i]);
+                    tmp = tmp.Next;
+                }
+
+                Length = array.Length;
+            }
+            else
+            {
+                Length = 0;
+                _root = null;
+            }
+        }
+
+        public void Add(int value)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            Node tmp = _root;
+
+            while (tmp != null)
+            {
+                str += _root.Value + ";";
+                tmp = tmp.Next;
+            }
+            return base.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            LinkedList linkedList = (LinkedList)obj;
+
+            if(Length != linkedList.Length)
+            {
+                return false;
+            }
+            Node tmp1 = _root;
+            Node tmp2 = linkedList._root;
+
+            while (tmp1 != null || tmp2 != null)
+            {
+                if(tmp1.Value != tmp2.Value)
+                {
+                    return false;
+                }
+                tmp1 = tmp1.Next;
+                tmp2 = tmp2.Next;
+            }
+
+            return true;
+        }
     }
 }
--- DataStructureTests/LinkedListTest.cs
+++ DataStructureTests/LinkedListTest.cs
@@ -1,10 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DataStructure.LinkedLists;
+using System;
+using NUnit.Framework;
 using System.Text;
 
 namespace DataStructure.Tests
 {
-    class LinkedListTest
+    public class LinkedListTest
     {
+        [TestCase(3, new int[] { 1, 2, 3, 4, 5 }, 4)]
+        [TestCase(0, new int[] { 0 }, 0)]
+        [TestCase(5, new int[] { 1, 4, 5, 3, 7, 4 }, 4)]
+        public void GetIndexatorTest(int index, int[] array, int expected)
+        {
+            LinkedList actualList = new LinkedList(array);
+            int actual;
+
+            actual = actualList[index];
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(-1, new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(1, new int[] { 0 })]
+        [TestCase(43, new int[] { 1, 4, 5, 3, 7, 4 })]
+        [TestCase(0, new int[] { })]
+        public void GetIndexatorNegativeTest(int index, int[] array)
+        {
+            LinkedList actualList = new LinkedList(array);
+            int actual;
+
+            Assert.Throws<IndexOutOfRangeException>(() => actual = actualList[index]);
+        }
+
+        [TestCase(3, 10, new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 10, 5 })]
+        [TestCase(0, 1, new int[] { 0 }, new int[] { 1 })]
+        [TestCase(5, -1, new int[] { 1, 43, 4, 5, 2, 1 }, new int[] { 1, 43, 4, 5, 2, -1 })]
+        public void SetIndexatorTest(int index, int value, int[] array, int[] expectedArray)
+        {
+            LinkedList actual = new LinkedList(array);
+            LinkedList expected = new LinkedList(expectedArray);
+
+            actual[index] = value;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(-1, 0, new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(1, 0, new int[] { 0 })]
+        [TestCase(43, 0, new int[] { 1, 4, 5, 3, 7, 4 })]
+        [TestCase(0, 0, new int[] { })]
+        public void SetIndexatorNegativeTest(int index, int value, int[] array)
+        {
+            LinkedList actual = new LinkedList(array);
+
+            Assert.Throws<IndexOutOfRangeException>(() => actual[index] = value);
+        }
+
+        [TestCase(6, new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 4, 5, 6 })]
+        [TestCase(0, new int[] { }, new int[] { 0 })]
+        [TestCase(-4, new int[] { 1 }, new int[] { 1, -4 })]
+        public void AddTest(int value, int[] array, int[] expectedArray)
+        {
+            LinkedList actual = new LinkedList(array);
+            LinkedList expected = new LinkedList(expectedArray);
+
+            actual.Add(value);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(6, 10, new int[] { }, new int[] { 6, 6, 6, 6, 6, 6, 6, 6, 6, 6 })]
+        [TestCase(0, 5, new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 4, 5, 0, 0, 0, 0, 0 })]
+        [TestCase(-4, 3, new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 4, 5, -4, -4, -4 })]
+        public void AddFewElementsTest(int value, int quantity, int[] array, int[] expectedArray)
+        {
+            LinkedList actual = new LinkedList(array);
+            LinkedList expected = new LinkedList(expectedArray);
+
+            for (int i = 0; i < quantity; i++)
+            {
+                actual.Add(value);
+            }
+
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
